467dcc1a8e706dbd80ad6b426b330fbac5a16c00
Atharva Puranik <atharva.puranik@avantika.edu.in>
2021-05-27 14:25:14 +0530

    gui added with pygame

--- game.py
+++ game.py
@@ -1,17 +1,36 @@
-"""
-This module is a grid based game inspired from a game called Co Ganh
-It is played on a 5x5 grid
-It involves 2 players each with 8 pieces
-First one to capture 7 of the other pieces wins
-"""
-
+import pygame, sys
 import numpy as np
-ROW = 5
-COL = 5
-turn = 0
+
+pygame.init()
+
+ROWS, COLS = 5, 5
+WIDTH, HEIGHT = 500, 500
+CYAN = (0, 255, 255)
+GRID_LINES = ("#007575")
+P1_COLOR = ("#EAEB87")
+P2_COLOR = ("#FF8E9C")
+TURN = 0
 points_1 = 0
 points_2 = 0
-board = np.zeros((ROW, COL))  # creating a grid of zeroes
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Ganh')
+screen.fill(CYAN)
+
+board = np.zeros((ROWS, COLS))  # creating a grid of zeroes
+
+def grid(): # drawing grid on screen
+    # horizontal lines
+    pygame.draw.line(screen, GRID_LINES, (0, 100), (500, 100), 5)
+    pygame.draw.line(screen, GRID_LINES, (0, 200), (500, 200), 5)
+    pygame.draw.line(screen, GRID_LINES, (0, 300), (500, 300), 5)
+    pygame.draw.line(screen, GRID_LINES, (0, 400), (500, 400), 5)
+
+    # vertical lines
+    pygame.draw.line(screen, GRID_LINES, (100, 0), (100, 500), 5)
+    pygame.draw.line(screen, GRID_LINES, (200, 0), (200, 500), 5)
+    pygame.draw.line(screen, GRID_LINES, (300, 0), (300, 500), 5)
+    pygame.draw.line(screen, GRID_LINES, (400, 0), (400, 500), 5)
 
 def mark(row, col, player):  # marking positions on grid
     board[row][col] = player
@@ -33,7 +52,77 @@ def start():  # starting position
     mark(4, 4, 2)
     mark(4, 3, 2)
     mark(4, 2, 2)
-    print(board)
+
+
+def draw_figures():
+    for row in range(ROWS):
+        for col in range(COLS):
+            if board[row][col] == 1:
+                pygame.draw.circle(screen, P1_COLOR, (int(col * 100 + 100 // 2), int(row * 100 + 100 // 2)), 20, 0)
+            elif board[row][col] == 2:
+                pygame.draw.circle(screen, P2_COLOR, (int(col * 100 + 100 // 2), int(row * 100 + 100 // 2)), 20, 0)
+            elif board[row][col] == 0:
+                pygame.draw.circle(screen, CYAN, (int(col * 100 + 100 // 2), int(row * 100 + 100 // 2)), 20, 0)
+
+def piece():
+    global TURN
+    if TURN % 2 == 0:
+        if event.type == pygame.MOUSEBUTTONDOWN: #player1
+            if event.button == 1:
+                pos = pygame.mouse.get_pos()
+                row_p = int(pos[1] // 100)
+                col_p = int(pos[0] // 100)
+                if board[row_p][col_p] == 0 or board[row_p][col_p] == 2:
+                    print("Invalid move")
+                else:
+                    mark(row_p, col_p, 0)
+                    print(board)
+                    draw_figures()
+                    pygame.display.update()
+
+            else:
+                pos_n = pygame.mouse.get_pos()
+                row_n = int(pos_n[1] // 100)
+                col_n = int(pos_n[0] // 100)
+                if board[row_n][col_n] == 2:
+                    print("Invalid move")
+                else:
+                    mark(row_n, col_n, 1)
+                    capture_ganh_1(row_n, col_n)
+                    capture_chet_1(row_n, col_n)
+                    print(board)
+                    draw_figures()
+                    pygame.display.update()
+                    TURN += 1
+
+    else:
+        if event.type == pygame.MOUSEBUTTONDOWN: #player2
+            if event.button == 1:
+                pos = pygame.mouse.get_pos()
+                row_p = int(pos[1] // 100)
+                col_p = int(pos[0] // 100)
+                if board[row_p][col_p] == 0 or board[row_p][col_p] == 1:
+                    print("Invalid move")
+                else:
+                    mark(row_p, col_p, 0)
+                    print(board)
+                    draw_figures()
+                    pygame.display.update()
+
+            else:
+                pos_n = pygame.mouse.get_pos()
+                row_n = int(pos_n[1] // 100)
+                col_n = int(pos_n[0] // 100)
+                if board[row_n][col_n] == 1:
+                    print("Invalid move")
+                else:
+                    mark(row_n, col_n, 2)
+                    capture_ganh_2(row_n, col_n)
+                    capture_chet_2(row_n, col_n)
+                    print(board)
+                    draw_figures()
+                    pygame.display.update()
+                    TURN += 1
 
 def move(row_p, col_p, row_n, col_n):
     """
@@ -279,50 +368,19 @@ def capture_chet_2(row_n, col_n):
     except IndexError:
         pass
 
-def game_over():
-    """
-    Checks points of both players
-    If equal to or more than 7
-        Declares winner
-        Game over
-    Else returns False
-    """
-    if points_1 >= 7:
-        print("Player 1 Wins!!!")
-        print("Game Over")
-    elif points_2 >= 7:
-        print("Player 2 Wins!!!")
-        print("Game Over")
-    else:
-        return False
 
-def game(row_p, col_p, row_n, col_n):
-    """
-    Displays instructions and starts the game
-    While game over condition is not reached, calls move
-    """
-    print("Assignment-1 - Grid Based Game\n")
-    var = int(input("Enter 1 for instructions, 2 to start the game: "))
-    if var == 1:
-        print("\nInstructions\n")
-        print("1. You can move your pieces to any adjacent empty space.")
-        print("2. To capture an opponent's piece you have 2 options-")
-        print("    a) place a piece between 2 of the opponent's pieces in a single line.")
-        print("    b) cover the opponent's piece from 2 sides in a single line.")
-        print("3. Each piece you capture gives you 1 point.")
-        print("4. First one to capture 7 or more pieces wins.\n")
-        start()
-        while game_over() is False:
-            move(row_p, col_p, row_n, col_n)
-    elif var == 2:
-        start()
-        while game_over() is False:
-            move(row_p, col_p, row_n, col_n)
-    else:
-        print("Enter a valid input. Try again.")
 
-row1 = 0
-col1 = 0
-row2 = 0
-col2 = 0
-game(row1, col1, row2, col2)  # calls for the game to start
+grid()
+start()
+draw_figures()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        piece()
+
+
+
+    pygame.display.update()
